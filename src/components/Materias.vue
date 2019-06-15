4a69d4c627ec1f97719dfdb4e7c465465b9f963d
Amabilie <amabilie.macedo@hotmail.com>
2019-06-15 17:58:12 -0300

    'update'

--- src/components/Materias.vue
+++ src/components/Materias.vue
@@ -1,147 +1,77 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
+        <v-img :src="require('../assets/materia.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
 
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to matérias
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">Cadastro de Matérias</h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
         </p>
       </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <v-card>
+          <!-- <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img> -->
 
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Cadastro de Matérias</h3>
+            </div>
+          </v-card-title>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+          <v-card-actions>
+            <v-flex>
+              <v-text-field v-model="email" :rules="emailRules" label="nome da disciplina" required></v-text-field>
+            </v-flex>
+            <v-btn flat color="orange" @click="teste()">Incluir</v-btn>
+          </v-card-actions>
+          <v-card-actions>
+            <v-flex>
+              <v-list>
+                <v-list-tile v-for="materia in materias" :key="materia.title" :value="materias">
+                  <v-list-tile-content>
+                    <v-list-tile-title v-text="materia.title"></v-list-tile-title>
+                  </v-list-tile-content>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
+                  <v-list-tile-avatar>
+                    <img :src="materia.avatar">
+                  </v-list-tile-avatar>
+                </v-list-tile>
+              </v-list>
+            </v-flex>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+var materiaService = require("../service/MateriaService");
 
-      ]
-    })
+export default {
+  name: "Alunos",
+  data() {
+    return {
+      materias: materiaService.list()
+    };
+  },
+  methods: {
+    teste: () => {
+      console.log(this.materias);
+    }
   }
+};
 </script>
 
 <style>
-
 </style>
